8ebdc2aa29d092a2eaeb13fdbb9612268fa1b5e5
SicoeRazvan <sikoe.razvan@gmail.com>
2025-03-14 22:23:43 +0200

    change auth in app.jsx

--- dream-journal/src/App.jsx
+++ dream-journal/src/App.jsx
@@ -13,30 +13,38 @@ import {
 import { db } from "./firebase";
 import Header from "./Components/Header";
 import About from "./Components/About";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function App() {
   const [dreams, setDreams] = useState([]);
+  const [user, setUser] = useState(null);
   const auth = getAuth();
-  const user = auth.currentUser;
 
   useEffect(() => {
-    
-      const q = query(
-        collection(db, "dreams"),
-        where("userId", "==", user?.uid),
-        orderBy("date", "desc")
-      );
-      const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
-        let dreamsArr = [];
-        QuerySnapshot.forEach((doc) => {
-          dreamsArr.push({ ...doc.data(), id: doc.id });
-        });
-        setDreams(dreamsArr);
+    const unsubscribeAuth = onAuthStateChanged(auth, (authUser) => {
+      setUser(authUser); 
+    });
+
+    return () => unsubscribeAuth(); 
+  }, [auth]);
+
+  useEffect(() => {
+    if (!user) return;
+
+    const q = query(
+      collection(db, "dreams"),
+      where("userId", "==", user.uid),
+      orderBy("date", "desc")
+    );
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let dreamsArr = [];
+      QuerySnapshot.forEach((doc) => {
+        dreamsArr.push({ ...doc.data(), id: doc.id });
       });
-      return () => unsubscribe();
-    
-  }, []);
+      setDreams(dreamsArr);
+    });
+    return () => unsubscribe();
+  }, [user]);
 
   return (
     <BrowserRouter>
